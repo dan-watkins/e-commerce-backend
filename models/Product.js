67a09290a5ac0a5e0925ea39e7c591d4f7c7bd9f
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-09-14 21:33:36 -0500

    cleaned up comments

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,6 @@ const sequelize = require("../config/connection");
 
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
-// The `/api/products` endpoint
-
-// get all products
 router.get("/", async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
@@ -17,10 +12,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-// get one product
 router.get("/:id", async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
@@ -39,14 +31,6 @@ router.get("/:id", async (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -108,13 +92,11 @@ router.put("/:id", (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
       where: {
